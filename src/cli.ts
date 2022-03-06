e9bad9f4007aa1ef816ce264842b2be174078c26
Rupert Rutledge <1982481+Eosis@users.noreply.github.com>
2022-03-06 10:02:05 +0000

    Another example

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
-import { foo } from './main'
+import { share_replay_example, behavior_subject_example } from './main'
 
-foo()
+if (process.argv[2] == 'behavior') {
+  console.log('Behaviour subject example')
+  behavior_subject_example()
+} else {
+  console.log('Share replay example')
+  share_replay_example()
+}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 
-import { timeout, of, BehaviorSubject } from 'rxjs'
-
-const base_subject$ = new BehaviorSubject(1);
-
-const with_timeout = base_subject$.pipe(
-	timeout({first: 500, with: () => of(-1)}),
-);
+import { timeout, of, BehaviorSubject, shareReplay, Observable} from 'rxjs'
 
 const wait = async (ms: number) => {
   await new Promise((resolve) => { setTimeout(resolve, ms) })
 }
 
-export const foo = async (): Promise<void> => {
-  const sub = with_timeout.subscribe(
+export const behavior_subject_example = async (): Promise<void> => {
+  const base_subject$ = new BehaviorSubject(1)
+
+  const with_timeout$ = base_subject$.pipe(
+    timeout({first: 500, with: () => of(-1)}),
+  );
+
+  const sub = with_timeout$.subscribe(
     console.log
   )
   await wait(1000)
@@ -20,3 +20,27 @@ export const foo = async (): Promise<void> => {
   await wait(1000)
   sub.unsubscribe()
 }
+
+export const share_replay_example = async (): Promise<void> => {
+  const long_running_observable$ = new Observable((subscriber) => {
+    setTimeout(() => subscriber.next(1), 1000)
+  });
+
+  const with_replay$ = long_running_observable$.pipe(
+    shareReplay(1),
+  )
+
+  const with_timeout$ = with_replay$.pipe(
+    timeout({first: 1500, with: () => of(-1)}),
+  )
+
+  const first_sub$ = with_timeout$.subscribe((x) => console.log(`First Sub: ${x}`))
+  await wait(2500);
+  const second_sub$ = with_timeout$.subscribe((x) => console.log(`Second Sub: ${x}`))
+  await wait(2500);
+
+  first_sub$.unsubscribe()
+  second_sub$.unsubscribe()
+}
+
+
